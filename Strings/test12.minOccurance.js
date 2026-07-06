cb02a94026d487cc3e975db47cb69fae192c37e1
Suman Patra <sumanpatra6274@gmail.com>
2026-07-06 17:17:03 +0530

    some more programms on string

---
+++ Strings/test12.minOccurance.js
@@ -0,0 +1,25 @@
+let str = "banana" ;
+let strArr = [...str] ;
+let min = Number.MAX_SAFE_INTEGER ;
+let ans = ""
+
+for(let i = 0 ; i< strArr.length ;i++){
+    let count = 1 ;
+    if(strArr[i] == ""){
+        continue ;
+    }
+
+    for(let j = i+1 ;j<strArr.length ;j++){
+        if(strArr[i] == strArr[j]){
+            count ++ ;
+            strArr[j] = "" ;
+        }
+    }
+
+    if(count < min){
+        min = count ;
+        ans = strArr[i]
+    }
+}
+
+console.log(ans, min);
