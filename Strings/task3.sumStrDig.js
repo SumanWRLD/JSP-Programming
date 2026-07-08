5477db5df6faadd5b753661b2af4fbd06f754031
Suman Patra <sumanpatra6274@gmail.com>
2026-07-08 17:30:03 +0530

    some more strinng programs

--- Strings/task3.sumStrDig.js
+++ Strings/task3.sumStrDig.js
@@ -1,13 +1,9 @@
-function isNum(ch){
-    return parseInt(ch)
-}
-
 let str = "sum999an" ;
 let strArr = [...str] ;
 let sum = 0 ;
 
 for(let i = 0 ; i<strArr.length ;i++){
-    if(isNum(str[i])){
+    if(parseInt(str[i])){
         sum = sum + Number(str[i])
     }
 }
